00b69e5d685d15684eaf9bb624dc42eb3ff700b6
Cooper.T <tanqiseng@gmail.com>
2020-01-16 17:39:10 +0800

    init proj
    Signed-off-by: Cooper.T <tanqiseng@gmail.com>
    Signed-off-by: kelvin <kelvin@meituan.com>
    Signed-off-by: tanqisen <tanqiseng@gmail.com>
    Signed-off-by: Cooper.T <tanqiseng@gmail.com>

---
+++ index.js
@@ -0,0 +1,634 @@
+let STMT_Break = function (label) { this.label = label }
+let STMT_Continue = function (label) { this.label = label }
+let STMT_Return = function (value) { this.value = value }
+
+let Interper = function (native) {
+    this.native = native
+    this.globalScope = { vars: {} }
+    this.scope = this.globalScope
+}
+
+Interper.prototype.execute = function (code) {
+    this.code = code
+
+    let ast
+    if (typeof module === 'object' && module.exports) {
+        ast = require('./acorn').parse(code)
+    } else {
+        ast = acorn.parse(code)
+    }
+
+    return this.step(ast)
+}
+
+Interper.prototype.step = function (node) {
+    let warks = {
+        Program: this.stepProgram,
+        Identifier: this.stepIdentifier,
+        Literal: this.stepLiteral,
+        RegExpLiteral: this.stepRegExpLiteral,
+        ExpressionStatement: this.stepExpressionStatement,
+        BlockStatement: this.stepBlockStatement,
+        EmptyStatement: this.stepEmptyStatement,
+        DebuggerStatement: this.stepDebuggerStatement,
+        WithStatement: this.stepWithStatement,
+        ReturnStatement: this.stepReturnStatement,
+        LabeledStatement: this.stepLabeledStatement,
+        BreakStatement: this.stepBreakStatement,
+        ContinueStatement: this.stepContinueStatement,
+        Choice: this.stepChoice,
+        IfStatement: this.stepIfStatement,
+        SwitchStatement: this.stepSwitchStatement,
+        SwitchCase: this.stepSwitchCase,
+        Exceptions: this.stepExceptions,
+        ThrowStatement: this.stepThrowStatement,
+        TryStatement: this.stepTryStatement,
+        CatchClause: this.stepCatchClause,
+        WhileStatement: this.stepWhileStatement,
+        DoWhileStatement: this.stepDoWhileStatement,
+        ForStatement: this.stepForStatement,
+        ForInStatement: this.stepForInStatement,
+        FunctionDeclaration: this.stepFunctionDeclaration,
+        VariableDeclaration: this.stepVariableDeclaration,
+        VariableDeclarator: this.stepVariableDeclarator,
+        Expressions: this.stepExpressions,
+        ThisExpression: this.stepThisExpression,
+        ArrayExpression: this.stepArrayExpression,
+        ObjectExpression: this.stepObjectExpression,
+        Property: this.stepProperty,
+        FunctionExpression: this.stepFunctionExpression,
+        UnaryExpression: this.stepUnaryExpression,
+        UpdateExpression: this.stepUpdateExpression,
+        BinaryExpression: this.stepBinaryExpression,
+        AssignmentExpression: this.stepAssignmentExpression,
+        LogicalExpression: this.stepLogicalExpression,
+        MemberExpression: this.stepMemberExpression,
+        ConditionalExpression: this.stepConditionalExpression,
+        CallExpression: this.stepCallExpression,
+        NewExpression: this.stepNewExpression,
+        SequenceExpression: this.stepSequenceExpression,
+        Pattern: this.stepPattern
+    }
+    let executor = (warks[node['type']]).bind(this, node)
+    return executor()
+}
+
+let Reference = function (obj, propName) {
+    this.obj = obj
+    this.name = propName
+}
+
+Reference.prototype.setValue = function (value) {
+    this.obj[this.name] = value
+}
+
+Reference.prototype.getValue = function () {
+    if (this.name instanceof Function) {
+        return this.name
+    }
+
+    if (this.obj === undefined || this.obj === null) {
+        return undefined
+    }
+
+    if (this.name === undefined || this.name === null) {
+        return undefined
+    }
+
+    return this.obj[this.name]
+}
+
+Interper.prototype.getReferenceOfNode = function (node) {
+    if (node.type === 'Identifier') {
+        let obj = this.getScopeForName(node.name)
+        if (obj && obj !== this.native) {
+            obj = obj.vars
+        }
+        return new Reference(obj, node.name)
+    } else if (node.type === 'MemberExpression') {
+        let obj = this.step(node.object)
+        let name = this.getNodeName(node)
+        return new Reference(obj, name)
+    } else if (node.type === 'FunctionExpression') {
+        return new Reference(this.native, this.createFunction(node))
+    } else {
+        return null
+    }
+}
+
+Interper.prototype.getScopeForName = function (name) {
+    let scope = this.scope
+    let parentScope = null
+
+    while (scope) {
+        if (this.hasVar(name, scope)) {
+            return scope
+        }
+
+        if (scope.environment) {
+            scope = scope.environment
+            if (!parentScope) {
+                parentScope = scope.parent
+            }
+        } else {
+            scope = scope.parent
+        }
+    }
+
+    while (parentScope) {
+        if (this.hasVar(name, parentScope)) {
+            return parentScope
+        }
+        parentScope = parentScope.parent
+    }
+
+    if (this.native && this.native.hasOwnProperty(name)) {
+        return this.native
+    }
+
+    return undefined
+}
+
+Interper.prototype.defineVar = function (name, value, scope) {
+    scope = scope || this.scope
+    scope.vars[name] = value
+}
+
+Interper.prototype.getVar = function (name, scope) {
+    scope = scope || this.scope
+    return scope.vars[name]
+}
+
+Interper.prototype.hasVar = function (name, scope) {
+    scope = scope || this.scope
+    return scope.vars.hasOwnProperty(name)
+}
+
+Interper.prototype.getNodeName = function (node) {
+    if (node.type === 'Identifier') {
+        return node.name
+    } else if (node.type === 'Literal') {
+        return node.value
+    } else if (node.type === 'MemberExpression') {
+        return node.computed ? this.step(node.property) : node.property.name
+    } else {
+        throw new Error()
+    }
+}
+
+Interper.prototype.executeInNewScope = function (func) {
+    this.scope = { parent: this.scope, vars: {} }
+    let value = func(this.scope)
+    this.scope = this.scope.parent
+
+    return value
+}
+
+Interper.prototype.isStmtControlFlow = function (value) {
+    return (value instanceof STMT_Break || value instanceof STMT_Continue || value instanceof STMT_Return)
+}
+
+Interper.prototype.createFunction = function (node) {
+    let self = this
+    let environment = this.scope
+
+    let func = function (...args) {
+        return self.executeInNewScope(scope => {
+            self.hoistDeclarations(node.body, scope)
+
+            scope.environment = environment
+
+            node.params.forEach(paramNode => {
+                self.defineVar(paramNode.name, args.shift())
+            })
+
+            for (statement of node.body.body) {
+                let stmtValue = self.step(statement)
+                if (stmtValue instanceof STMT_Return) {
+                    return stmtValue.value
+                }
+            }
+        })
+    }
+
+    func._vm_function_ = true
+    return func
+}
+
+Interper.prototype.hoistDeclarations = function (node, scope) {
+    if (!node) {
+        return
+    }
+
+    switch (node.type) {
+        case 'FunctionDeclaration': {
+            if (!this.hasVar(scope)) {
+                this.defineVar(node.id.name, this.createFunction(node), scope)
+            }
+            break
+        }
+        case 'VariableDeclaration': {
+            if (node.kind === 'var') {
+                for (let declaration of node.declarations) {
+                    this.defineVar(this.getNodeName(declaration.id, undefined, scope))
+                }
+            }
+            break
+        }
+        case 'Program': {
+            for (let statement of node.body) {
+                this.hoistDeclarations(statement, scope)
+            }
+            break
+        }
+        case 'BlockStatement': {
+            for (let block of node.body) {
+                this.hoistDeclarations(block, scope)
+            }
+            break
+        }
+        case 'WhileStatement': {
+            this.hoistDeclarations(node.body, scope)
+            break
+        }
+        case 'DoWhileStatement': {
+            this.hoistDeclarations(node.body, scope)
+            break
+        }
+        case 'ForStatement': {
+            this.hoistDeclarations(node.init, scope)
+            this.hoistDeclarations(node.body, scope)
+            break
+        }
+        case 'ForInStatement': {
+            this.hoistDeclarations(node.left, scope)
+            this.hoistDeclarations(node.body, scope)
+            break
+        }
+        case 'IfStatement': {
+            this.hoistDeclarations(node.consequent, scope)
+            this.hoistDeclarations(node.alternate, scope)
+            break
+        }
+        default: break
+    }
+}
+
+Interper.prototype.stepProgram = function (node) {
+    this.hoistDeclarations(node, this.scope)
+
+    let value
+    node.body.forEach(element => {
+        let ret = this.step(element)
+        if (element.type !== 'FunctionDeclaration' && element.type !== 'VariableDeclaration') {
+            value = ret
+        }
+    });
+
+    return value
+}
+
+Interper.prototype.stepIdentifier = function (node) {
+    return this.getReferenceOfNode(node).getValue()
+}
+Interper.prototype.stepLiteral = function (node) {
+    return node.value
+}
+Interper.prototype.stepRegExpLiteral = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepExpressionStatement = function (node) {
+    return this.step(node.expression)
+}
+Interper.prototype.stepBlockStatement = function (node) {
+    return this.executeInNewScope(_ => {
+        let stmtValue
+        for (let element of node.body) {
+            stmtValue = this.step(element)
+            if (this.isStmtControlFlow(stmtValue)) {
+                return stmtValue
+            }
+        }
+        return stmtValue
+    })
+}
+Interper.prototype.stepEmptyStatement = function (node) {
+}
+Interper.prototype.stepDebuggerStatement = function (node) {
+    debugger;
+}
+Interper.prototype.stepWithStatement = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepReturnStatement = function (node) {
+    if (node.argument) {
+        return new STMT_Return(this.step(node.argument))
+    }
+    return new STMT_Return()
+}
+Interper.prototype.stepLabeledStatement = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepBreakStatement = function (node) {
+    return new STMT_Break(node.label)
+}
+Interper.prototype.stepContinueStatement = function (node) {
+    return new STMT_Continue(node.label)
+}
+Interper.prototype.stepChoice = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepIfStatement = function (node) {
+    let condition = this.step(node.test)
+
+    return this.executeInNewScope(_ => {
+        if (condition) {
+            return this.step(node.consequent)
+        } else {
+            if (node.alternate) {
+                return this.step(node.alternate)
+            }
+        }
+    })
+}
+Interper.prototype.stepSwitchStatement = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepSwitchCase = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepExceptions = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepThrowStatement = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepTryStatement = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepCatchClause = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepWhileStatement = function (node) {
+    let stmtValue
+    while (this.step(node.test)) {
+        stmtValue = this.executeInNewScope(_ => {
+            return this.step(node.body)
+        })
+
+        if (stmtValue instanceof STMT_Break) {
+            break
+        } else if (stmtValue instanceof STMT_Continue) {
+            continue
+        } else if (stmtValue instanceof STMT_Return) {
+            return stmtValue
+        }
+    }
+    return stmtValue
+}
+Interper.prototype.stepDoWhileStatement = function (node) {
+    let stmtValue
+    do {
+        stmtValue = this.executeInNewScope(_ => {
+            return this.step(node.body)
+        })
+
+        if (stmtValue instanceof STMT_Continue) {
+            continue
+        } else if (stmtValue instanceof STMT_Break) {
+            break
+        } else if (stmtValue instanceof STMT_Return) {
+            return stmtValue
+        }
+    } while (this.step(node.test))
+
+    return stmtValue
+}
+Interper.prototype.stepForStatement = function (node) {
+    return this.executeInNewScope(_ => {
+        if (node.init) {
+            this.step(node.init)
+        }
+
+        let stmtValue
+        while (this.step(node.test)) {
+            let stmtValue = this.step(node.body)
+            this.step(node.update)
+
+            if (stmtValue instanceof STMT_Continue) {
+                continue
+            } else if (stmtValue instanceof STMT_Break) {
+                break
+            } else if (stmtValue instanceof STMT_Return) {
+                return stmtValue
+            }
+        }
+        return stmtValue
+    })
+}
+Interper.prototype.stepForInStatement = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepFunctionDeclaration = function (node) {
+    if (!this.hasVar(node.id.name)) {
+        this.defineVar(node.id.name, this.createFunction(node))
+    }
+}
+Interper.prototype.stepVariableDeclaration = function (node) {
+    node.declarations.forEach(element => {
+        let ref = this.getReferenceOfNode(element.id)
+
+        if (node.kind !== 'var' || !ref) {
+            this.step(element)
+        } else {
+            ref.setValue(element.init ? this.step(element.init) : undefined)
+        }
+    })
+}
+Interper.prototype.stepVariableDeclarator = function (node) {
+    let name = this.getNodeName(node.id)
+
+    if (!this.hasVar(name)) {
+        this.defineVar(name, undefined)
+    }
+
+    if (node.init) {
+        this.defineVar(name, this.step(node.init))
+    }
+}
+Interper.prototype.stepExpressions = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepThisExpression = function (node) {
+    return this._curThis
+}
+Interper.prototype.stepArrayExpression = function (node) {
+    return node.elements.map(element => this.step(element))
+}
+Interper.prototype.stepObjectExpression = function (node) {
+    let obj = {}
+    node.properties.forEach(prop => {
+        let key = this.getNodeName(prop.key);
+        obj[key] = this.step(prop.value)
+    })
+    return obj
+}
+Interper.prototype.stepProperty = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepFunctionExpression = function (node) {
+    return this.createFunction(node)
+}
+Interper.prototype.stepUnaryExpression = function (node) {
+    let value = this.step(node.argument)
+
+    switch (node.operator) {
+        case '-': return -value;
+        case '+': return +value;
+        case '!': return !value;
+        case '~': return ~value;
+        case 'typeof': return typeof value;
+        case 'void': return void value;
+        case 'delete': return delete value;
+    }
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepUpdateExpression = function (node) {
+    let ref = this.getReferenceOfNode(node.argument)
+    let value = ref.getValue()
+    let updatedValue = value
+
+    if (node.operator === '++') {
+        updatedValue++
+    } else if (node.operator === '--') {
+        updatedValue--
+    } else {
+        throw new SyntaxError()
+    }
+
+    ref.setValue(updatedValue)
+    return node.prefix ? updatedValue : value
+}
+Interper.prototype.stepBinaryExpression = function (node) {
+    let leftValue = this.step(node.left)
+    let rightValue = this.step(node.right)
+
+    switch (node.operator) {
+        case '==': return leftValue == rightValue
+        case '!=': return leftValue != rightValue
+        case '===': return leftValue === rightValue
+        case '!==': return leftValue !== rightValue
+        case '<': return leftValue < rightValue
+        case '<=': return leftValue <= rightValue
+        case '>': return leftValue > rightValue
+        case '>=': return leftValue >= rightValue
+        case '<<': return leftValue << rightValue
+        case '>>': return leftValue >> rightValue
+        case '>>>': return leftValue >>> rightValue
+        case '+': return leftValue + rightValue
+        case '-': return leftValue - rightValue
+        case '*': return leftValue * rightValue
+        case '/': return leftValue / rightValue
+        case '%': return leftValue % rightValue
+        case '|': return leftValue | rightValue
+        case '^': return leftValue ^ rightValue
+        case '&': return leftValue & rightValue
+        case 'in': return leftValue in rightValue
+        case 'instanceof': return leftValue instanceof rightValue
+        default: throw new Error('')
+    }
+
+}
+Interper.prototype.stepAssignmentExpression = function (node) {
+    const rightValue = this.step(node.right)
+    let ref = this.getReferenceOfNode(node.left)
+    let value = ref.getValue()
+
+    switch (node['operator']) {
+        case '=': value = rightValue; break;
+        case '+=': value += rightValue; break;
+        case '-=': value -= rightValue; break;
+        case '*=': value *= rightValue; break;
+        case '/=': value /= rightValue; break;
+        case '%=': value %= rightValue; break;
+        case '<<=': value <<= rightValue; break;
+        case '>>=': value >>= rightValue; break;
+        case '>>>=': value >>>= rightValue; break;
+        case '&=': value &= rightValue; break;
+        case '^=': value ^= rightValue; break;
+        case '|=': value |= rightValue; break;
+        default:
+            throw SyntaxError('Unknown assignment expression: ' + node['operator']);
+    }
+
+    ref.setValue(value)
+    return value
+}
+Interper.prototype.stepLogicalExpression = function (node) {
+    let leftValue = this.step(node.left)
+    let rightValue = this.step(node.right)
+
+    switch (node.operator) {
+        case '||': return leftValue || rightValue
+        case '&&': return leftValue && rightValue
+        default: throw SyntaxError(`Unknown operator: ${node.operator}`)
+    }
+}
+Interper.prototype.stepMemberExpression = function (node) {
+    return this.getReferenceOfNode(node).getValue()
+}
+Interper.prototype.stepConditionalExpression = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepCallExpression = function (node) {
+    let args = node.arguments.map(exp => this.step(exp))
+
+    let obj
+    let callee
+
+    if (node.callee.type === 'CallExpression') {
+        callee = this.step(node.callee)
+        obj = this.native
+    } else {
+        let funcRef = this.getReferenceOfNode(node.callee)
+        callee = funcRef.getValue()
+        obj = funcRef.obj
+    }
+
+    let preThis = this._curThis
+    this._curThis = obj
+    let ret = callee.apply(obj, args)
+    this._curThis = preThis
+
+    return ret
+}
+Interper.prototype.stepNewExpression = function (node) {
+    let func = this.getReferenceOfNode(node.callee).getValue()
+    let args = node.arguments.map(exp => this.step(exp))
+
+    if (func._vm_function_) {
+        let obj = Object.create(func.prototype)
+        let preThis = this._curThis
+        this._curThis = obj
+
+        func.apply(obj, args)
+
+        this._curThis = preThis
+
+        return obj
+    } else {
+        return new func(args)
+    }
+}
+Interper.prototype.stepSequenceExpression = function (node) {
+    throw new Error(`${node.type}`)
+}
+Interper.prototype.stepPattern = function (node) {
+    throw new Error(`${node.type}`)
+}
+
+if (typeof module === 'object' && module.exports) {
+    module.exports = Interper
+} else {
+    Interper = Interper
+}
---
+++ test/index.js
@@ -0,0 +1,405 @@
+var Interper = require("../index.js");
+var assert = require("assert");
+var fs = require('fs');
+
+function _eval(code) {
+	let interper = new Interper(global)
+	return interper.execute(code)
+}
+
+function test(code) {
+	assert.equal(_eval(code), eval(code))
+}
+
+it('hoisting 5', function () {
+	test(`
+	function hoistVariable() {
+		var foo = 10
+		console.log(foo)  // 10
+		if (true) {
+			var foo = 5;
+			console.log(foo) // 5， 局部作用域重新定义，但覆盖到函数级别
+		}
+
+		console.log(foo); // 5
+		foo
+	}
+
+	hoistVariable();
+	`)
+})
+
+it('hoisting 4', function () {
+	test(`
+	function hoistVariable() {
+		if (!foo) {
+			var foo = 5;
+		}
+
+		return foo
+	}
+
+	hoistVariable();
+	`)
+})
+
+it('hoisting 3', function () {
+	test(`
+		a
+		var a = 1
+		a
+	`)
+})
+
+it('hoisting 2', function () {
+	test(`
+		a = 10
+		console.log(a) // a = 10
+		if (false) {
+			var a   // var 会提升变量声明，let不会
+		}
+	`)
+})
+
+it('hoisting 1', function () {
+	test(`
+		sum(1,2)
+		function sum(a, b) { return a + b }
+	`)
+})
+
+it('closure 8', function () {
+	test(`
+	(function (a) {
+  		return function(b) {
+      		{
+            	let e = 'ee'
+         		return function(c) {
+             		{  
+                		let f = 'ff'
+                		return function(d) {
+                    		return a + b + c + d + e + f
+                		}
+             		}
+         		}
+      		}
+  		}
+	})('aa')('bb')('cc')('dd')
+	`)
+})
+
+it('closure 7', function () {
+	test(`
+let h = 'hh';
+(function (a) {
+  return function(b) {
+      {
+            let e = 'ee'
+         return function(c) {
+             {  
+                let f = 'ff'
+                return function(d) {
+                    return a + b + c + d + e + f + g + h
+                }
+                var g = 'gg'
+             }
+         }
+      }
+  }
+})('aa')('bb')('cc')('dd')
+	`)
+})
+
+it('closure 6', function () {
+	test(`
+		(function (a) {
+  			return function(b) {
+     			return function(c) {
+					 return function(d) {
+							return a + b + c + d
+					 }
+     			}
+  			}
+		})('aa')('bb')('cc')('dd')
+	`)
+})
+
+it('closure 5', function () {
+	test(`
+		function makeWorker() {
+  			let name = "Pete";
+
+  			return function() {
+				  return name
+  			};
+		}
+
+		let name = "John";
+		let work = makeWorker();
+		work()
+	`)
+})
+
+it('closure 4', function () {
+	test(`
+		function add() {
+   			let a = 1;
+  			const addOne = function(b) { return b + a; }
+  			++a;
+    		return addOne;
+		}
+		const addOne = add();
+		addOne(1)
+	`)
+})
+
+
+it('closure 3', function () {
+	test(`
+		let a = {name: 'Tom'}
+		let func = (function (obj) {
+			return function() {
+				return obj.name
+			}
+		})(a)
+		//a = {name: 'Sam'}
+		a.name = 'Sam'
+		func()
+	`)
+})
+
+it('closure 2', function () {
+	test(`
+		let a = {name: 'Tom'}
+		let func = (function (obj) {
+			return function() {
+				return obj.name
+			}
+		})(a)
+		a = {name: 'Sam'}
+		func()
+	`)
+})
+
+it('closure 1', function () {
+	test(`
+		(function (obj) {
+			return function () {
+				return obj.abc;
+			};
+		})({abc: 'xxxyy'})()
+	`)
+})
+
+it('md5', function () {
+	var code = fs.readFileSync('test/md5.js', { encoding: 'UTF-8' });
+	code += `
+		md5('202cb962ac59075b964b07152d234b70')
+	`
+	test(code)
+})
+
+it('ObjectExpression', function () {
+	let obj = _eval(`
+		let obj ={
+			a: 1,
+			'b': 2
+		}
+		obj
+	`)
+	assert.equal(obj.a, 1)
+	assert.equal(obj.b, 2)
+})
+
+it('new', function () {
+	test(`
+		let Persion = function(name, age) {
+			this.name = name
+			this.age = age
+		}
+		Persion.prototype.sayName = function() {return this.name}
+		Persion.prototype.sayAge = function() {return this.age}
+		let persion = new Persion('Tom', 23)
+
+		persion instanceof Persion
+	`)
+})
+
+it('function 6: anonymous funtions', function () {
+	test(`(function(a, b){return a + b })(100, 2)`)
+})
+
+it('function 5', function () {
+	let persion = _eval(`
+		let Persion = function(name, age) {
+			this.name = name
+			this.age = age
+		}
+		Persion.prototype.sayName = function() {return this.name}
+		Persion.prototype.sayAge = function() {return this.age}
+		let persion = new Persion('Tom', 23)
+
+		let ret = {
+			'persion': persion,
+			name : persion.sayName(),
+			age : persion.sayAge()
+		}
+		ret
+	`)
+	assert.equal(persion.name, 'Tom')
+	assert.equal(persion.persion.name, 'Tom')
+	assert.equal(persion.age, 23)
+	assert.equal(persion.persion.age, 23)
+})
+
+it('function 4', function () {
+	test(`
+		let Car = function() {}
+		Car.prototype.constructor === Car
+	`)
+})
+
+it('function 3', function () {
+	test(`
+		let Car = function() {}
+		Car.prototype.constructor === Car
+	`)
+})
+
+it('function 2', function () {
+	test(`
+		function test() {
+			return 123
+		}
+
+		test(101,11)
+	`)
+})
+
+it('function declaration', function () {
+	test(`
+		function sum(a,b) {
+			return a+b
+		}
+
+		sum(101, 20)
+	`)
+})
+
+it('native call/new native object', function () {
+	test(`
+		let str = new String('abcdef')
+		str.toUpperCase()
+	`)
+})
+
+it('native call', function () {
+	test(`
+		let n = parseInt('123')
+		n
+	`)
+})
+
+it('variable declaration', function () {
+	test(`
+		var a
+		a = 'abc'
+	`)
+})
+
+it('EmptyStatement', function () {
+	test(`
+		let a = 100
+		{
+			;
+		}
+		a
+	`)
+})
+
+it('update: ++', function () {
+	test(`
+		var i = 1
+		var b = (++i)+0
+		b
+	`)
+})
+
+it('update: ++', function () {
+	test(`
+		var i = 1
+		var c = (i++)+0
+		c
+	`)
+})
+
+it('assigment', function () {
+	let b = _eval(`
+		let b = [1,2,3]
+		{
+    		b['a'] = 'tt'
+		}
+		b
+	`)
+	assert.equal(b[0], 1)
+	assert.equal(b[1], 2)
+	assert.equal(b[2], 3)
+	assert.equal(b['a'], 'tt')
+})
+
+it('native call', function () {
+	_eval(` console.log('111aaa') `)
+})
+
+it("while", function () {
+	test(`
+		var _global_a = 0
+		while(true) {
+			_global_a += 1
+
+			if (_global_a < 100) {
+				continue
+			} 
+			break
+		}
+
+		_global_a
+	`)
+});
+
+it('new/this', function () {
+	let myCar = _eval(`
+		let Car = function(make, model, year) {
+			this.make = make
+			this.model = model
+			this.year = year
+		}
+
+		//Car.prototype.getYear = function() {
+  //			return this.year
+		//}
+
+		let myCar = new Car('Ford', 'Mustang', '1969')
+		myCar
+	`)
+	assert.equal(myCar['make'], 'Ford')
+	assert.equal(myCar['model'], 'Mustang')
+	assert.equal(myCar['year'], '1969')
+})
+
+it('AssignmentExpression', function () {
+	test(`let a = 1; a = 'abc1'`)
+})
+
+it('WhileStatement', function () {
+	test(`let a = 3, b=10; while(a>=0){b+=a; a--; b}`)
+})
+
+it('ifStatement', function () {
+	test(`if(true){let a=1, b=4; a+b}`)
+})
+
+it('ExpressionStatement', function () {
+	test(`1+2`)
+})
+
